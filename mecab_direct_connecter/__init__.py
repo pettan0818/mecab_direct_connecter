df1f117d068540e1560e61a413a1745177ff02c0
pettan0818 <pettan0818+git@gmail.com>
2016-10-21 17:44:27 +0900

    test2

--- mecab_direct_connecter/__init__.py
+++ mecab_direct_connecter/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from mecab_direct_connecter import mecab_direct_connecter
+from mecab_direct_connecter.mecab_direct_connecter import mecab_direct_connecter
 __author__ = "pettan0818"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
