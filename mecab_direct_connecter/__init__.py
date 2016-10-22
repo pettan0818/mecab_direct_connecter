58b6157ebfd1ca987b37b510a774678a16b158a5
pettan0818 <pettan0818+git@gmail.com>
2016-10-22 18:45:40 +0900

    Properly import local env...

--- mecab_direct_connecter/__init__.py
+++ mecab_direct_connecter/__init__.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from mecab_direct_connecter.mecab_direct_connecter import MecabMother
+from mecab_direct_connecter.mecab_direct_connecter import *
+
+__all__ = ["MecabMother", "StopWordKiller"]
+
 __author__ = "pettan0818"
 __version__ = "0.0.2"
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from mecab_direct_connecter import __version__
 
 setup(
-    name = "mecab_direct_connecter",
-    version = __version__,
+    name="mecab_direct_connecter",
+    version=__version__,
     description="Easily Mecab Wrapper",
-    author = "pettan0818",
-    packages = ["mecab_direct_connecter"],
-    install_requires = [
+    author="pettan0818",
+    packages=["mecab_direct_connecter"],
+    install_requires=[
         "mecab-python3",
         "neologdn",
         "pandas"
-    ]
+    ],
+    package_data={"mecab_direct_connecter": ["mecab_direct_connecter/stop_words.data"]}
 )
