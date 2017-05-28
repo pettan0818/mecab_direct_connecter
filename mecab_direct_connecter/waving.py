a1ae900d4730893f24e232e06080d132c24a754a
pettan0818 <pettan0818+git@gmail.com>
2017-05-29 01:21:51 +0900

    [waving] filtering waving words.

--- mecab_direct_connecter/waving.py
+++ mecab_direct_connecter/waving.py
@@ -12,6 +12,25 @@
 # Usage
 #
 """
+import pickle
+
+
+def waving_words_filter(waving_dic_path: str, word_list: list):
+    """filtering word_list by using check waving word dictionary.
+
+    >>> waving_words_filter("./waving_dic.dic", ["お子様", "子供", "子ども"])
+    ["子ども", "子ども", "子ども"]
+    >>> waving_words_filter("./waving_dic.dic", ["テスト"])
+    ['Test1']
+    """
+    waving_dic = load_dic_pickled_data(waving_dic_path)
+
+    return [waving_dic.get(word, word) for word in word_list]
+
+
+def load_dic_pickled_data(path: str):
+    """reading dictionary data from path."""
+    return pickle.load(open(path, "rb"))
 
 
 if __name__ == '__main__':
