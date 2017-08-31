ceedd010c31259347cb76ec0e2943240b2f14a9c
pettan0818 <pettan0818+git@gmail.com>
2017-09-01 03:57:51 +0900

    [english filter] basic file added.

---
+++ mecab_direct_connecter/lang_distincter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+
+"""
+#
+# Author:   Noname
+# URL:      https://github.com/pettan0818
+# License:  MIT License
+# Created: 木  8/31 06:42:32 2017
+
+# Usage
+#
+"""
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
