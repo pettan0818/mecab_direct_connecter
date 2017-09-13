a97caf4a8a3f55b7047e330969dc147842c2e8bb
pettan0818 <pettan0818+git@gmail.com>
2017-09-14 01:06:22 +0900

    [init, api] settings now corrected.

--- mecab_direct_connecter/__init__.py
+++ mecab_direct_connecter/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from .api import morph, setup, setup_path
+from .api import MopheUnit, setup, setup_path, jpn_morph
 from .language import lang_parser
 from .morphing import MecabMother
 from .stopword import StopWordKiller
 from .waving import waving_words_filter
 
-__all__ = ["morphing", "stopword", "waving", "language"]
+__all__ = ["morphing", "stopword", "waving", "language", "api"]
 
 __author__ = "pettan0818"
 __version__ = "0.2.0_beta"
